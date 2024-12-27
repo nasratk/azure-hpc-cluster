17c6e97dcb41cb6a268b880ae546884fdb9206f9
Nasrat Kamal <nasrat.kamal@gmail.com>
2024-12-27 00:20:18 +0000

    Update install-excel.ps1

--- install-excel.ps1
+++ install-excel.ps1
@@ -6,20 +6,24 @@ Invoke-WebRequest -Uri $odtUrl -OutFile $odtPath
 # Extract the ODT
 Start-Process -FilePath $odtPath -ArgumentList "/quiet /extract:$env:TEMP" -Wait
 
-# Create the configuration XML file for Excel Standalone Volume License
+# Create the configuration XML file for Excel with all prompts suppressed
 $configXml = @"
 <Configuration>
-  <Add SourcePath="$env:TEMP" OfficeClientEdition="64">
-    <Product ID="Excel2021Volume" >
+  <Add OfficeClientEdition="64" Channel="Monthly">
+    <Product ID="Excel2021Retail">
       <Language ID="en-us" />
     </Product>
   </Add>
   <Display Level="None" AcceptEULA="TRUE" />
   <Property Name="AUTOACTIVATE" Value="1" />
+  <Property Name="SharedComputerLicensing" Value="1" />
+  <Property Name="PinIconsToTaskbar" Value="FALSE" />
+  <Property Name="SCLCacheOverride" Value="0" />
+  <Property Name="DisableFirstRunWizard" Value="TRUE" />
 </Configuration>
 "@
 $configXmlPath = "$env:TEMP\configuration.xml"
 $configXml | Out-File -FilePath $configXmlPath -Encoding UTF8
 
-# Install Excel
+# Install Excel silently
 Start-Process -FilePath "$env:TEMP\setup.exe" -ArgumentList "/configure $configXmlPath" -Wait
