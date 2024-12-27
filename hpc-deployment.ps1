d3acbed584d287b7680c9e67e946b7ab6c28ca75
Nasrat Kamal <your_email>
2024-12-27 01:13:56 +0000

    experimental changes

--- hpc-deployment.ps1
+++ hpc-deployment.ps1
@@ -1,8 +1,8 @@
 #az login
 #az account set -s "da883fd8-76c8-4ca5-a7e6-8567ae83ce10"
-az group create -l uksouth -n rg-hpcuk-1
+az group create -l uksouth -n rg-hpcuk-t
 az deployment group create `
     --name hpc-cluster-deployment `
-    --resource-group rg-hpcuk-1 `
+    --resource-group rg-hpcuk-t `
     --template-file ./hpc-cluster-template.json `
     --parameters ./hpc-cluster-parameters.json
